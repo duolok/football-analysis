596fc655be1a8404b6e0333903d642cd9905dcc8
duolok <dusan.lecic5@gmail.com>
2024-11-15 19:34:05 +0100

    add: binary for cleaning up data

---
+++ src/bin/process_events.rs
@@ -0,0 +1,22 @@
+use football_analysis::handlers;
+use football_analysis::validation;
+use dotenv::dotenv;
+use sqlx::PgPool;
+use std::sync::Arc;
+use std::env;
+
+const JSONL_PATH: &str = "events.jsonl";
+
+#[tokio::main]
+async fn main() -> Result<(), sqlx::Error> {
+    dotenv().ok(); // load env variables
+    let database_url = env::var("DATABASE_URL").expect("DATABASE_URL must be set");
+    println!("{}", &database_url);
+
+    let pool = Arc::new(PgPool::connect(&database_url).await?);
+    println!("Connected to PostgreSQL!");
+
+    handlers::process_events(JSONL_PATH, pool).await;
+
+    Ok(())
+}
