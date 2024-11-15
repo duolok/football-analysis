f22121dec84c2920f90e8fde41c775568aef7307
duolok <dusan.lecic5@gmail.com>
2024-11-15 10:35:27 +0100

    add: event models

---
+++ src/models.rs
@@ -0,0 +1,40 @@
+use serde::{Deserialize, Serialize};
+use serde_json::Value;
+
+#[derive(Debug, Deserialize)]
+pub struct Event {
+    pub event_id:   i32,
+    pub event_timestamp: i64,
+    pub event_type: EventType,
+    pub event_data: Value,
+}
+
+#[derive(Debug, Deserialize)]
+#[serde(rename_all = "lowercase")]
+pub enum EventType {
+    Registration,
+    SessionPing,
+    Match
+}
+
+#[derive(Debug, Deserialize)]
+pub struct RegistrationData {
+    pub country: String,
+    pub user_id: String,
+    pub device_os: String,
+}
+
+#[derive(Debug, Deserialize)]
+pub struct SessionPingData {
+    pub user_id: String,
+    pub session_type: Option<String>
+}
+
+#[derive(Debug, Deserialize)]
+pub struct MatchData {
+    pub match_id: String,
+    pub home_user_id: String,
+    pub away_user_id: String,
+    pub home_goals_scored: Option<i32>,
+    pub away_goals_scored: Option<i32>,
+}
